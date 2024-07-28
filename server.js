ae45e2679cb7cba07ce88367fd21c02708e8db8e
Nantha <nanthu1593@gmail.com>
2024-07-28 23:46:47 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-const express = require
+const express = require("express");
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send("App is ready");
+})
+
+
+app.get('/working',(req,res)=>{
+    res.send("Cool its working");
+})
+
+app.get('/ready',(req,res)=>{
+    res.send("It works");
+})
+
+
+app.listen(3000,()=>{
+    console.log("App connected in 3000")
+})
